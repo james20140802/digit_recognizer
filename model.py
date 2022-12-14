65ab5840984f33eb9f92d8e401a28300c3ace468
SangHyup Kim <drchasekim@gmail.com>
2022-12-14 19:03:16 +0900

    Create model.py. In this file the model for this project will be created.

---
+++ model.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+
+class Model(tf.keras.Model):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(32, 3, activation='relu')
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense1 = tf.keras.layers.Dense(128, activation='relu')
+        self.dense2 = tf.keras.layers.Dense(10, activation='softmax')
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.dense1(x)
+        x = self.dense2(x)
+        return x
